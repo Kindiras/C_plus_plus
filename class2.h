4aa37b9d66bf6a21e9505f3abe7f0e8bac352ed4
Indiras Khatri <42945839+Kindiras@users.noreply.github.com>
2019-09-05 16:36:43 -0400

    Update class2.h

--- class2.h
+++ class2.h
@@ -11,7 +11,7 @@ class IndiClass
 {
     public:
 //This is the constructor has the same name as the class.
-// It can neither return or even void.         
+// It can neither return nor even void.         
 	    IndiClass(string x, double y, double z, string col);
 	    void get_detal();
 	    string getName();
